82415e87fe84d5773fd656b1cb5408caa51fd564
OscarShen <oscarshen09@foxmail.com>
2017-08-29 14:41:40 +0800

    graphics buffer

---
+++ core/render/graphics_buffer.cpp
@@ -0,0 +1,135 @@
+#include "graphics_buffer.h"
+#include <base/context.h>
+#include <base/util.h>
+#include "mapping.h"
+#include "render_engine.h"
+namespace gleam {
+	GraphicsBuffer::GraphicsBuffer(BufferUsage usage, uint32_t access_hint, uint32_t size_in_byte)
+		: usage_(usage), access_hint_(access_hint), size_in_byte_(size_in_byte)
+	{
+	}
+	GraphicsBuffer::~GraphicsBuffer()
+	{
+		this->DeleteResource();
+	}
+	OGLGraphicsBuffer::OGLGraphicsBuffer(BufferUsage usage, uint32_t access_hint, GLenum target, uint32_t size_in_byte, ElementFormat format)
+		: GraphicsBuffer(usage, access_hint, size_in_byte), vbo_(0), texture_(0),target_(target), format_(format)
+	{
+		assert((GL_ARRAY_BUFFER == target) || (GL_ELEMENT_ARRAY_BUFFER == target) || (GL_UNIFORM_BUFFER == target));
+	}
+	OGLGraphicsBuffer::~OGLGraphicsBuffer()
+	{
+		this->DeleteResource();
+	}
+	void OGLGraphicsBuffer::CopyToBuffer(GraphicsBuffer & rhs)
+	{
+		glCopyNamedBufferSubData(vbo_, checked_cast<OGLGraphicsBuffer*>(&rhs)->vbo_, 0, 0, size_in_byte_);
+	}
+	void OGLGraphicsBuffer::CreateResource(void const * init_data)
+	{
+		assert(vbo_ == 0);
+		glCreateBuffers(1, &vbo_);
+
+		GLenum usage;
+		if (usage_ == BU_Static)
+		{
+			if (access_hint_ & EAH_CPU_Read)
+			{
+				usage = GL_STATIC_READ;
+			}
+			else
+			{
+				usage = GL_STATIC_DRAW;
+			}
+		}
+		else
+		{
+			assert(usage_ == BU_Dynamic);
+			if (access_hint_ & EAH_CPU_Read)
+			{
+				usage = GL_DYNAMIC_READ;
+			}
+			else
+			{
+				usage = GL_DYNAMIC_DRAW;
+			}
+		}
+		
+		glNamedBufferData(vbo_, static_cast<GLsizeiptr>(size_in_byte_), init_data, usage);
+
+		if ((access_hint_ & EAH_GPU_Read) && (format_ != EF_Unknown))
+		{
+			GLint internal_format;
+			GLenum gl_format;
+			GLenum gl_type;
+			OGLMapping::MappingFormat(internal_format, gl_format, gl_type, format_);
+
+			glCreateTextures(GL_TEXTURE_BUFFER, 1, &texture_);
+			glTextureBuffer(texture_, internal_format, vbo_);
+		}
+	}
+	void OGLGraphicsBuffer::DeleteResource()
+	{
+		if (texture_ != 0)
+		{
+			glDeleteTextures(1, &texture_);
+			texture_ = 0;
+		}
+
+		if (vbo_ != 0)
+		{
+			if (Context::Instance().RenderEngineValid())
+			{
+				OGLRenderEngine &re = *checked_cast<OGLRenderEngine*>(&Context::Instance().RenderEngineInstance());
+				re.DeleteBuffers(1, &vbo_);
+			}
+			else
+			{
+				glDeleteBuffers(1, &vbo_);
+			}
+			vbo_ = 0;
+		}
+	}
+	void OGLGraphicsBuffer::Active(bool force)
+	{
+		OGLRenderEngine &re = *checked_cast<OGLRenderEngine*>(&Context::Instance().RenderEngineInstance());
+		re.BindBuffer(target_, vbo_, force);
+	}
+	void * OGLGraphicsBuffer::Map(BufferAccess ba)
+	{
+		OGLRenderEngine &re = *checked_cast<OGLRenderEngine*>(&Context::Instance().RenderEngineInstance());
+
+		void *p;
+		if ((ba == BA_Write_Only) && (BU_Dynamic == usage_))
+		{
+			GLuint access = GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT;
+			p = glMapNamedBufferRange(vbo_, 0, static_cast<GLsizeiptr>(size_in_byte_), access);
+		}
+		else
+		{
+			GLenum flag = 0;
+			switch (ba)
+			{
+			case gleam::BA_Read_Only:
+				flag = GL_READ_ONLY;
+				break;
+			case gleam::BA_Write_Only:
+				flag = GL_WRITE_ONLY;
+				break;
+			case gleam::BA_Read_Write:
+				flag = GL_READ_WRITE;
+				break;
+			case gleam::BA_Write_No_Overwrite:
+				CHECK_INFO(false, "no impl...");
+				break;
+			}
+
+			p = glMapNamedBuffer(vbo_, flag);
+		}
+		return p;
+	}
+	void OGLGraphicsBuffer::Unmap()
+	{
+		glUnmapNamedBuffer(vbo_);
+	}
+}
---
+++ core/render/graphics_buffer.h
@@ -0,0 +1,84 @@
+/************************************************************************ 
+ * @description :  
+ * @author		:  $username$
+ * @creat 		:  $time$
+************************************************************************ 
+ * Copyright @ OscarShen 2017. All rights reserved. 
+************************************************************************/  
+#pragma once
+#ifndef GLEAM_CORE_GRAPHICS_BUFFER_H_
+#define GLEAM_CORE_GRAPHICS_BUFFER_H_
+#include <GL/glew.h>
+#include <gleam.h>
+#include "element_format.h"
+namespace gleam
+{
+	enum BufferUsage
+	{
+		BU_Static,
+		BU_Dynamic
+	};
+	enum BufferAccess
+	{
+		BA_Read_Only,
+		BA_Write_Only,
+		BA_Read_Write,
+		BA_Write_No_Overwrite
+	};
+
+	class GraphicsBuffer
+	{
+	public:
+		GraphicsBuffer(BufferUsage usage, uint32_t access_hint, uint32_t size_in_byte);
+		virtual ~GraphicsBuffer();
+
+		uint32_t Size() const { return size_in_byte_; }
+		BufferUsage Usage() const { return usage_; }
+		uint32_t AccessHint() const { return access_hint_; }
+
+		virtual void CopyToBuffer(GraphicsBuffer& rhs) = 0;
+
+		virtual void CreateResource(void const * init_data) = 0;
+		virtual void DeleteResource() = 0;
+
+	private:
+		virtual void* Map(BufferAccess ba) = 0;
+		virtual void Unmap() = 0;
+
+	protected:
+		BufferUsage usage_;
+		uint32_t access_hint_;
+		uint32_t size_in_byte_;
+	};
+
+	class OGLGraphicsBuffer : public GraphicsBuffer
+	{
+	public:
+		explicit OGLGraphicsBuffer(BufferUsage usage, uint32_t access_hint, GLenum target,
+			uint32_t size_in_byte, ElementFormat format);
+		~OGLGraphicsBuffer();
+
+		void CopyToBuffer(GraphicsBuffer& rhs) override;
+
+		void CreateResource(void const * init_data) override;
+		void DeleteResource() override;
+
+		void Active(bool force);
+
+		GLuint GLvbo() const { return vbo_; }
+		GLuint GLTexture() const { return texture_; }
+		GLenum GLType() const { return target_; }
+
+	private:
+		void* Map(BufferAccess ba) override;
+		void Unmap() override;
+
+	private:
+		GLuint vbo_;
+		GLuint texture_;
+		GLenum target_;
+		ElementFormat format_; // format for shader resource
+	};
+}
+
+#endif // !GLEAM_CORE_GRAPHICS_BUFFER_H_
