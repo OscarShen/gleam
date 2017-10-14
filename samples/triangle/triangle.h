1208ecf65e0efa994435de9d787726daab7d376f
OscarShen <oscarshen09@foxmail.com>
2017-10-14 14:01:52 +0800

    first triangle sample

---
+++ samples/triangle/triangle.h
@@ -0,0 +1,46 @@
+/************************************************************************ 
+ * @description :  
+ * @author		:  $username$
+ * @creat 		:  $time$
+************************************************************************ 
+ * Copyright @ OscarShen 2017. All rights reserved. 
+************************************************************************/  
+#pragma once
+#ifndef GLEAM_SAMPLE_TRIANGLE_H_
+#define GLEAM_SAMPLE_TRIANGLE_H_
+
+#include <render/renderable.h>
+#include <scene/scene_object.h>
+#include <base/framework.h>
+namespace gleam
+{
+	class RenderTriangle : public RenderableHelper
+	{
+	public:
+		RenderTriangle();
+	};
+
+	class TriangleObject : public SceneObjectHelper
+	{
+	public:
+		TriangleObject()
+			: SceneObjectHelper(std::make_shared<RenderTriangle>(), SOA_Cullable)
+		{
+		}
+	};
+
+	class TriangleApp : public Framework3D
+	{
+	public:
+		TriangleApp();
+
+	private:
+		void OnCreate() override;
+		uint32_t DoUpdate(uint32_t render_index) override;
+
+	private:
+		SceneObjectPtr polygon_;
+	};
+}
+
+#endif // !GLEAM_SAMPLE_TRIANGLE_H_
